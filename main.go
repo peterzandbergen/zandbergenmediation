647acaa2b9109653db49663e34110f23eb3a1e03
peterzandbergen <peter.zandbergen@myhops.com>
2018-08-26 18:54:21 +0200

    base domain redirected to www. prefixed domain

--- main.go
+++ main.go
@@ -7,8 +7,11 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"strings"
 
 	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
 )
 
 func main() {
@@ -19,6 +22,55 @@ func main() {
 
 func handle(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, brave new world!")
+}
+
+type prefixHandler struct {
+	from string
+	to   string
+	h    http.Handler
+}
+
+func (ph *prefixHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	log.Debugf(ctx, "prefix. Host is: %s", r.Host)
+	// Split port and host.
+	s := strings.Split(r.Host, ":")
+	log.Debugf(ctx, "prefix. elements: %d", len(s))
+	log.Debugf(ctx, "prefix. s: %v", s)
+
+	// Test if hosts does not start with from.
+	if strings.ToLower(s[0]) != strings.ToLower(ph.from) {
+		// Call original handler.
+		log.Debugf(ctx, "prefix. calling original handler")
+		ph.h.ServeHTTP(w, r)
+		return
+	}
+
+	// Build the url to redirect
+	s[0] = ph.to
+	log.Debugf(ctx, "prefix. modified s: %v", s)
+	u := *r.URL
+	u.Host = strings.Join(s, ":")
+	log.Debugf(ctx, "prefix. modified url: %s", u.String())
+	http.Redirect(w, r, u.String(), http.StatusMovedPermanently)
+}
+
+func HostPrefixHandlerFunc(prefix string, handlerFunc http.HandlerFunc) http.HandlerFunc {
+	h := handlerFunc
+	p := prefix
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Test if hosts starts with prefix.
+		host := r.URL.Host
+		if strings.HasPrefix(host, p) {
+			// Call original handler.
+			h.ServeHTTP(w, r)
+		} else {
+			// Build the url to redirect
+			u := *r.URL
+			u.Host = p + u.Host
+			http.Redirect(w, r, u.String(), http.StatusMovedPermanently)
+		}
+	}
 }
 
 func NewServeViewFunc(view string) http.HandlerFunc {
@@ -33,13 +85,22 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func RegisterHandlers() {
+	// Create a servemutex for the routing.
+	sm := http.NewServeMux()
 	indexHandlerFunc := NewServeViewFunc(indexView)
-	http.HandleFunc("/index.html", indexHandlerFunc)
-	http.HandleFunc("/contact.html", NewServeViewFunc(contactView))
-	http.HandleFunc("/mediation.html", NewServeViewFunc(mediationView))
-	http.HandleFunc("/nmi.html", NewServeViewFunc(nmiView))
-	http.HandleFunc("/uwmediator.html", NewServeViewFunc(uwMediatorView))
-	// http.HandleFunc("/sitemap.xml", handleSitemap)
-	http.HandleFunc("/", indexHandlerFunc)
+	sm.HandleFunc("/index.html", indexHandlerFunc)
+	sm.HandleFunc("/contact.html", NewServeViewFunc(contactView))
+	sm.HandleFunc("/mediation.html", NewServeViewFunc(mediationView))
+	sm.HandleFunc("/nmi.html", NewServeViewFunc(nmiView))
+	sm.HandleFunc("/uwmediator.html", NewServeViewFunc(uwMediatorView))
+	sm.HandleFunc("/sitemap.xml", handleSitemap)
+	sm.HandleFunc("/", indexHandlerFunc)
+	// Prefix handler handles the requests before sm.
+	ph := &prefixHandler{
+		from: os.Getenv("URL_FROM"),
+		to:   os.Getenv("URL_TO"),
+		h:    sm,
+	}
+	// Regiser the handler for all paths.
+	http.Handle("/", ph)
 }
-
